46eef56b928626754f09cc278fbf686fad560bfc
ymy1224 <choiibabyovo@gmail.com>
2024-05-13 15:49:40 +0900

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,7 +31,7 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Computer Science",
   "User experience",
   "Inclusive design",
   "Focus group testing",

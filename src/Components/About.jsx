d46980ce075d07e01f65724dec19c85e78cebd6b
ymy1224 <choiibabyovo@gmail.com>
2024-05-13 15:53:37 +0900

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "我是yumingyu 在测试";
 
 const About = () => {
   return (

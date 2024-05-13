2a3ff42bd54b384f2e3ca3320375813981eb3b02
ymy1224 <choiibabyovo@gmail.com>
2024-05-13 06:27:04 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "YU MINGYU",
+  title: "Student",
+  email: "yumingy24@naver.com",
+  gitHub: "yumingyu",
+  instagram: "yumingyu",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
+  twitter: "yumingyu",
   youTube: "Code",
 };
 
